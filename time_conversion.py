67218df00ff7a68bdeb259a93ada7632b825951c
Bushan Bengaluru <bushan@Mac.attlocal.net>
2025-01-03 16:24:07 -0500

    new script

---
+++ time_conversion.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytz
+
+def convert_time(src_time, src_tz, dest_tz):
+    src_tz = pytz.timezone(src_tz)
+    dest_tz = pytz.timezone(dest_tz)
+    src_time = src_tz.localize(src_time)
+    dest_time = src_time.astimezone(dest_tz)
+    return dest_time
+
+def main():
+    src_time = datetime.now()
+    src_tz = 'EST'
+    dest_tz = 'INDIAN/MAURITIUS'
+    dest_time = convert_time(src_time, src_tz, dest_tz)
+    print(f"TIME IN INDIA IS: {dest_time.strftime('%Y-%m-%d %H:%M')}")
+
+if __name__ == '__main__':
+    main()
